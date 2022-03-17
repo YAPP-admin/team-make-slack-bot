807b89947f265361a7f559bb3f2167869633c30f
Ji-Ha <xodhkd36@naver.com>
2022-03-17 15:02:22 +0900

    feat: 팀원 분배 패키지 구현

---
+++ divide.py
@@ -0,0 +1,28 @@
+import properties
+
+def divid_member(members):
+        d_members = []
+        total_member = len(members)
+        # set을 활용한 무작위 진행
+        members = set(members)
+        members = list(members)
+        
+        if total_member < properties.LIMITED_NUMBER_OF_PEOPLE:
+            return list(members)
+        
+        # limit만큼 데이터를 집어넣어서 list를 만든다.
+        # list를 모으는 list를 만든다.
+        # 저장된 데이터를 return한다.
+        while len(members) >= properties.LIMITED_NUMBER_OF_PEOPLE:
+            temp_list = []
+            for i in range(0, properties.LIMITED_NUMBER_OF_PEOPLE):
+                temp_list.append(members.pop())
+            print(temp_list)
+            d_members.append(temp_list)
+        
+        for member_list in d_members:
+            if len(members) <= 0: 
+                break
+            member_list.append(members.pop())
+        
+        return d_members
