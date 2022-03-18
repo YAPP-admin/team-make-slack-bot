07976cda9debc3b2768eb1be4a9befac33cb5e0a
Ji-Ha <xodhkd36@naver.com>
2022-03-18 16:06:23 +0900

    refactor: members가 하나 들어올 때 type을 list로 변환

--- divide.py
+++ divide.py
@@ -1,18 +1,22 @@
 import properties
 
 def divid_member(members):
+        if type(members) is str:
+            members = list(members)
+        
         d_members = []
-        total_member = len(members)
+    
         # set을 활용한 무작위 진행
         members = set(members)
         members = list(members)
+        total_member = len(members)
         
         if total_member < properties.LIMITED_NUMBER_OF_PEOPLE:
             return list(members)
         
         # limit만큼 데이터를 집어넣어서 list를 만든다.
         # list를 모으는 list를 만든다.
-        # 저장된 데이터를 return한다.
+        # 저장된 데이터를 return한다.        
         while len(members) >= properties.LIMITED_NUMBER_OF_PEOPLE:
             temp_list = []
             for i in range(0, properties.LIMITED_NUMBER_OF_PEOPLE):
