0d5636c5c73b5c3654501ff24ddeb3c91137f779
Ji-Ha <xodhkd36@naver.com>
2022-03-18 16:07:00 +0900

    refactor(startMessage): 채널이 존재하지 않을 때 예외처리 추가

--- startMessage.py
+++ startMessage.py
@@ -3,5 +3,8 @@ import properties
 
 slack = dedenziBot.SlackAPI()
 
-ts = slack.post_init_message(properties.CHANNEL_NAME)
-print(ts)
+try:
+    ts = slack.post_init_message(properties.CHANNEL_NAME)
+    print(ts)
+except:
+    print("없는 채널이에요! 채널 이름을 다시 설정하세요!")
