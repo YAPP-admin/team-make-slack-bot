4b11b84dd5a99a26141cc7e69d4b82ae4946374d
s2moon98 <s2moon98@naver.com>
2022-04-16 17:56:00 +0900

    slack bot start, end api

--- app/services/startMessage.py
+++ app/services/startMessage.py
@@ -1,10 +1,11 @@
-import dedenziBot
+from . import dedenziBot
 import properties
 
-slack = dedenziBot.SlackAPI()
+def startMessage():
+    slack = dedenziBot.SlackAPI()
 
-try:
-    ts = slack.post_init_message(properties.CHANNEL_NAME)
-    print(ts)
-except:
-    print("없는 채널이에요! 채널 이름을 다시 설정하세요!")
+    try:
+        ts = slack.post_init_message(properties.CHANNEL_NAME)
+        print(properties.CHANNEL_NAME)
+    except:
+        print("없는 채널이에요! 채널 이름을 다시 설정하세요!")
